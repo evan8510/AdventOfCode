99bb33a21b55a119c197537ff8d898a1d0081ac0
Evan Wauchope <55203303+evan8510@users.noreply.github.com>
2022-12-09 00:40:16 -0500

    Update Day3.java
    formatting

--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -12,7 +12,7 @@ public class Day3 {
 		HashSet<Character> items = new HashSet<Character>();
 		int sum = 0;
 		int p = 2; 		// 1 -> p1 solution
-						// 2 -> p2 solution
+					// 2 -> p2 solution
 
 		if (p == 1) { 	// Solution for p1
 
@@ -22,10 +22,10 @@ public class Day3 {
 				while (s.hasNextLine()) {
 					String[] data = s.nextLine().split(""); 					// split every letter
 					for (int i = 0; i < (data.length) / 2; i++) 
-						items.add(data[i].charAt(0)); 							// fill set with half the characters in line
+						items.add(data[i].charAt(0)); 						// fill set with half the characters in line
 					for (int i = (data.length) / 2; i < data.length; i++) {
 						if (items.contains(data[i].charAt(0))) { 				// if the second half of the line is in the set add to sum
-																				// otherwise keep looking for a duplicate
+															// otherwise keep looking for a duplicate
 							if (data[i].charAt(0) > 90) 						
 								sum += ((int) (data[i].charAt(0)) - 96);
 							else
@@ -54,27 +54,27 @@ public class Day3 {
 					if (counter % 3 == 0) {
 						for (int i = 0; i < data.length(); i++) 	// fill items with first line
 							items.add(data.charAt(i));
-					} else if (counter % 3 == 1) {					// fill badges with chars in items & line 2
+					} else if (counter % 3 == 1) {				// fill badges with chars in items & line 2
 						for (int i = 0; i < data.length(); i++) {
 							char temp = data.charAt(i);
 							if (items.contains(temp)) 
 								badges.add(temp);
 						}
-						items.clear();								// empty items, badges now holds the set to check against
+						items.clear();					// empty items, badges now holds the set to check against
 					} else if (counter % 3 == 2) {
 						for (int i = 0; i < data.length(); i++) {
 							char temp = data.charAt(i);
-							if (badges.contains(temp)) {			// add to sum priotiry of first match in line 3 and badges
+							if (badges.contains(temp)) {		// add to sum priotiry of first match in line 3 and badges
 								if ((int) temp > 90)
 									sum += ((int) (temp) - 96);
 								else
 									sum += ((int) (temp) - 38);
-								badges.clear();						// clear sets
+								badges.clear();			// clear sets
 								items.clear();
 							}
 						}
 					}
-					counter++; 										// increment counter
+					counter++; 						// increment counter
 				}
 				s.close();
 			} catch (FileNotFoundException e) {
